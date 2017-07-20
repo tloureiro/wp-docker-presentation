8a2b78e08bc3ccf6c62042e2334c41770471c7cf
Thiago Loureiro <escazi1@gmail.com>
2017-07-20 15:11:16 -0400

    :)

--- disposable-wp.sh
+++ disposable-wp.sh
@@ -3,3 +3,14 @@
 docker stop some-mysql some-wordpress
 docker run --rm --name some-mysql -e MYSQL_ROOT_PASSWORD=password -d mysql
 docker run --rm --name some-wordpress --link some-mysql:mysql -p 8080:80 -d wordpress
+
+
+
+
+
+
+
+
+
+
+

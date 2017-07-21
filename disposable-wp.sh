02461183460f47a003ebf6c5ff8b3f789031af12
Thiago Loureiro <escazi1@gmail.com>
2017-07-21 17:47:49 -0400

    :)

--- disposable-wp.sh
+++ disposable-wp.sh
@@ -4,8 +4,9 @@ docker stop some-mysql some-wordpress
 docker run --rm --name some-mysql -e MYSQL_ROOT_PASSWORD=password -d mysql
 docker run --rm --name some-wordpress --link some-mysql:mysql -p 8080:80 -d wordpress
 
+read -p "Press any key to destroy me"
 
-
+docker stop some-mysql some-wordpress
 
 
 
